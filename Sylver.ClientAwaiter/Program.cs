dd6d5c58dd2cb81bc62db6a74d05b77a916573dd
Filipe GOMES PEIXOTO <gomespeixoto.filipe@gmail.com>
2020-02-22 19:45:28 +0100

    Add project files.

---
+++ Sylver.ClientAwaiter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sylver.ClientAwaiter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
